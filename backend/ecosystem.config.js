9e91e2e585f0c548f2ab4abafdd7b4c75c3262dc
Hardban Records Lab <hardbanrecordslab.pl@gmail.com>
2026-02-11 17:34:50 +0100

    Fix_workspaces_and_remove_Supabase

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,7 +6,8 @@ module.exports = {
       instances: 1,
       exec_mode: "fork",
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+        PORT: 3003
       }
     }
   ]
